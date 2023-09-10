f767377446c21b8b35adcb4e299c922ab26095a6
ADV Indian coder <116486037+ADVindiancoder@users.noreply.github.com>
2023-09-09 22:53:56 -0700

    Create hellow.js
    hello
    
    Co-Authored-By: ADV Indian coder <116486037+advindiancoder@users.noreply.github.com>

---
+++ hellow.js
@@ -0,0 +1,10 @@
+function calculateSum(a, b) {
+    return a + b;
+  }
+  
+  const num1 = 5;
+  const num2 = 3;
+  const result = calculateSum(num1, num2);
+  
+  console.log(`The sum of ${num1} and ${num2} is ${result}.`);
+  
